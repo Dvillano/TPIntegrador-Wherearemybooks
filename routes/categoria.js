bb25bee4b45afe1a36e4a21ff9963b6da3b6b008
nsjuarez42 <nsjuarez42@gmail.com>
2021-05-17 15:31:06 -0300

    separacion de rutas para evitar escribir sobre el codigo de los demas

---
+++ routes/categoria.js
@@ -0,0 +1,25 @@
+
+/*toda la implementacion de los requests aca luego se agregan en index js*/
+
+//conexion a base de datos importada de index
+const {connection} = require('../index');
+
+const categoriaPost =function(req,res){
+
+}
+
+const categoriaGet =function (req,res){
+
+}
+
+const categoriaGetById = function(req,res){
+
+}
+
+const categoriaDeleteById = function(req,res){
+
+}
+
+module.exports={
+    categoriaPost,categoriaGet,categoriaGetById,categoriaDeleteById
+}
---
+++ routes/persona.js
@@ -0,0 +1,8 @@
+const post = function(req,res){
+
+}
+
+const get = function(req,res){
+
+}
+
