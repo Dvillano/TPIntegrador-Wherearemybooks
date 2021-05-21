e88b6ccdc05cc75beb975aeb20050f4ba77b7d22
dvillano <danie.villano@gmail.com>
2021-05-21 13:39:13 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+
+app.use(express.json());
+
+// Conexion a DB
+const connection = mysql.createConnection({
+ host:'localhost',
+ user:'root',
+ password:'',
+ database:'libreria'
+})
+
+// Verificar conexion a DB
+connection.connect( (error) => {
+    if (error){
+        throw error;
+    }
+    console.log('Conexion con la base de datos establecida');
+});
+
+
+//se exporta la conexion a la base de datos para que sea una sola en todo el backend y 
+//se pueda usar en los requests de cada categoria
+exports.connection = connection;
+
+
+//Rutas importadas 
+const {categoriaGetById} = require('./routes/categoria')
+const {categoriaPostPersona} = require('./routes/persona')
+
+
+//Requests de persona:
+//POST 
+app.post('/persona', categoriaPostPersona);
+
+//fin de requests Persona
+
+
+//Requests de Libro
+
+//fin de requests de libro
+
+
+//Requests de Categoria
+//app.get('/categoria/:id',categoriaGetById);
+//fin de requests de categoria
+
+app.listen(3000,()=>console.log('listening on port 3000'));
+
