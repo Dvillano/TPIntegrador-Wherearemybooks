64d1419fa97efefb8c36d6db935066077be8020b
IgnaRios <ignacio.rios@hotmail.es>
2021-07-20 19:14:35 -0300

    prueba react categoria

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import {Route, BrowserRouter as Router} from 'react-router-dom'
 
 import Navbar from './Components/PaginaInicio/Navbar'
 import PrestarLibro from './Components/Libro/PrestarLibro/PrestarLibro';
+import CategoriaForm from './Components/Categoria/CategoriaForm';
+
+
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
     <div className="App">
     <Navbar/>
      <Route path="/prestarLibro" component={PrestarLibro}/>
+     <Route path="/ingresarGenero" component={CategoriaForm}/>
     </div>
     </Router>
   );
--- frontend/src/Components/Categoria/CategoriaForm.jsx
+++ frontend/src/Components/Categoria/CategoriaForm.jsx
@@ -1,17 +1,57 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import shortid from "shortid";
 
 export default function CategoriaForm() {
     const [categoria, setCategoria] = useState('');
+    const [inputText, setInputText] = useState('');
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        alert(`You entered ${categoria}`)
+    const inputTextHandler = (e) => {
+        setInputText(e.target.value)
     }
+    
+    const postPersonaUrl = 'http://localhost:3000/categoria'
+    
+    useEffect( () => {
+        async function fetchData() {
+            const respuesta = await axios.post(postPersonaUrl, inputText);
+            console.log(respuesta.data)
+        } 
+        
+        fetchData();
+    }, []);
 
+    
+    function submitHandler(e) {
+        e.preventDefault();
+        setCategoria([
+            ...categoria,
+                {
+                    text: inputText,
+                    id: shortid.generate()
+                }
+        ])
+        
+        
+        //alert(`You entered ${categoria}`)
+    }
+    /*
     function handleChange(event){
         setCategoria(event.target.value)
-    }
+    }*/
+
     return (
+        <div>
+            <h3>Ingrese una nueva categoría</h3>
+            <form  action='/categoria' method='POST'>
+                <label>Categoría</label>
+                <input onChange={inputTextHandler} type='text' placeholder='ingrese aquí la nueva categoría'></input>
+                <button onClick={submitHandler} type='submit'>
+                        Agregar!
+                </button>
+            </form>
+        </div>
+        /*
         <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="categoriaInput">Género nuevo</label>
@@ -26,6 +66,7 @@ export default function CategoriaForm() {
                 <button type="submit">Agregar género</button>
             </div>
         </form>
+        */
     )
 }
 
