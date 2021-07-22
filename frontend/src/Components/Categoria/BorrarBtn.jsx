ce2531bff16a0fbfcbaa74d2d194073835754a07
Federico Luna <federicogluna@gmail.com>
2021-07-22 16:35:41 -0300

    Se agrega lista de géneros y boton para borrar categoria

---
+++ frontend/src/Components/Categoria/BorrarBtn.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function BorrarCategoria () {
+    return (
+        <button type='button' onclick={BorrarCategoria}>Borrar</button>
+    )
+}
--- frontend/src/Components/Categoria/CategoriaList.jsx
+++ frontend/src/Components/Categoria/CategoriaList.jsx
@@ -1,34 +1,45 @@
 import React from 'react';
 import axios from 'axios';
+import './list.css';
+import './BorrarBtn'
+import BorrarCategoria from './BorrarBtn';
 
 
 export default function CategoriaList() {
 
-    const [data, setData] = React.useState([]);
-
-    React.useEffect(() => {
-        async function fetchData() {
-        const respuesta = await axios.get('http://localhost:3000/categoria');
-            if (respuesta.status === 200) {
-            console.log(respuesta.data);
-            setData(respuesta.data.response);
-        }
-      };
-        fetchData()
-      }, []);
-
-
-        const lista = data.map((item)=>{
-            return (
-                <div className="list" key={item.id}>
-                    <div className="listGenero">{item.genero}</div> 
-                </div>
-            )
-        })
-            return (
-                <div className = 'container'>
-                  {lista}
-                </div>
-              )
-        }
+    const [ listado, setListado ] = React.useState([])
     
+    React.useEffect(() => {
+        try {
+            const myFetch = async () => {
+                const url = 'http://localhost:4200/categoria/'
+                const result = await axios.get(url);
+                if (result.status === 200) {
+                    setListado(result.data.respuesta)
+                }
+            }
+            myFetch()
+            }
+        
+        catch (error) {
+            console.log(error)
+        }
+        }, [])
+        
+        const ListaCategorias = () => {
+            return (
+                listado.map(item => {
+                    return <li key={item.ID} className="item">{item.genero} <BorrarCategoria /></li>
+                })
+            )
+        }
+            return (
+                <div className="list-container">
+                    <h3>Géneros</h3>
+                    <div className="list">
+                        <ListaCategorias /> 
+                    </div>
+                </div>
+    
+        )
+    }
