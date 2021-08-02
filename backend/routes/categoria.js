47f26a1cc15b461e387ea25a2f278dc03ccc3bb8
Fede <84208153+Federicoluna1@users.noreply.github.com>
2021-08-02 16:12:31 -0300

    Se actualizó la función categoriaPost

--- backend/routes/categoria.js
+++ backend/routes/categoria.js
@@ -8,18 +8,18 @@ const categoriaPost = async (req, res) => {
         if (!req.body.genero){
             throw new Error ('Falta enviar el genero');
         }
+
         const categoria = req.body.genero.toUpperCase();
+
         let consulta = await qy('SELECT id FROM categoria WHERE genero = ?',[categoria]);
+        
         if (consulta.length > 0 ){
             throw new Error('¡Esa categoria ya existe!');
         };
+        
         const query = 'INSERT INTO categoria (genero) VALUE (?)';
-        const postGenero = await qy(query, [categoria]);
-
-        const solicitudGenero = 'SELECT * FROM categoria WHERE genero=?'
-        const respuesta = await qy(solicitudGenero, [categoria])
-        res.status(200).send({'Respuesta': respuesta});
-        console.log(respuesta)
+        const respuesta = await qy(query, [categoria]);
+        res.status(200).send({"ID": respuesta.insertId, "categoria": categoria});
     }
     catch(e){
         console.error(e.message);
@@ -31,6 +31,7 @@ const categoriaGet = async (req, res) => {
     try{
         const query = 'SELECT * FROM categoria';
         const respuesta =  await qy(query);
+        
         if (respuesta.length == 0){
             throw new Error ('No hay categorias cargadas');
         }
@@ -46,6 +47,7 @@ const categoriaGetById = async (req, res) => {
     try{
         let query = 'SELECT * FROM categoria WHERE id = ?';
         let respuesta = await qy(query, [req.params.id]);
+        
         if(respuesta.length == 0 ){
             throw new Error ('Categoría no encontrada');
         };        
@@ -60,14 +62,17 @@ const categoriaGetById = async (req, res) => {
 const categoriaDeleteById = async (req, res) => {
     try {
         let consultaGeneroID = await qy('SELECT * FROM categoria WHERE id = ?', [req.params.id]);
+        
         if(consultaGeneroID.length == 0){
             throw new Error ('No existe genero con el ID indicado')
         }
         let query = 'SELECT * FROM libro WHERE genero_id = ?';
         let respuesta = await qy (query, [req.params.id]);
+        
         if (respuesta.length > 0 ) {
             throw new Error ('Esta categoría tiene productos asociados, no se puede borrar');
         }        
+        
         query =  'DELETE FROM categoria WHERE id = ?'; 
         respuesta = await qy(query, [req.params.id]) ;
         res.status(200).send({respuesta: 'Se ha borrado correctamente la categoría'});
